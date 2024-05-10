d48ca342ac3d111a2dae6bc0a6f51ce0448574fb
Srithar810 <159476971+Srithar810@users.noreply.github.com>
2024-05-10 10:58:36 +0530

    Update settings.py name

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['srithar.pythonanywhere.com']
 
 
 # Application definition
